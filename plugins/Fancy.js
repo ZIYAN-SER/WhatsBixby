0038c596c8a98ea35d071f68d008602987680bcf
Ripper Sir <92292759+RIPPER-SER@users.noreply.github.com>
2022-04-12 14:36:26 +0530

    Update Fancy.js

--- plugins/Fancy.js
+++ plugins/Fancy.js
@@ -1,6 +1,8 @@
-
-
-
+/* Copyright (C) 2022 RIPPER-SER.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+https://github.com/RIPPER-SER/bixbymowl
+*/
 
 
 

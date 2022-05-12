ec7757a40e6e3b7c1966f6b6d9fb615432a5d8d2
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-12 12:13:23 +0530

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -7,7 +7,7 @@ https://github.com/RIPPER-SER/bixbymowl
 const WhatsBixby = require('../events');
 const Heroku = require('heroku-client');
 const Config = require('../config');
-const {MessageType} = require('@adiwajshing/baileys');
+const {MessageType} = require('@ZIYAN-SER/WhatsBixby-Web-Api');
 const got = require('got');
 const fs = require('fs');
 const Db = require('./sql/plugin');

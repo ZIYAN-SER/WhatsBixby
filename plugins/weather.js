213f8fc2f0957c71d336c39db6dd16e14baf3838
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-12 16:00:15 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -7,7 +7,7 @@ WhatsAsena - Yusuf Usta
 */
 
 const Asena = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
+const {MessageType} = require('@ZIYAN-SER/WhatsBixby-Web-Api');
 const got = require('got');
 const Config = require('../config');
 

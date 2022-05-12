29a4554d629942ad1488c125cbd810c1e75bcb4a
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-12 12:18:49 +0530

    Update autofilter.js

--- plugins/autofilter.js
+++ plugins/autofilter.js
@@ -7,7 +7,7 @@ https://github.com/RIPPER-SER/bixbymowl
 
 const Bixby = require('../events');
 
-const {MessageType} = require('@adiwajshing/baileys');
+const {MessageType} = require('@ZIYAN-SER/WhatsBixby-Web-Api');
 
 const GM = "."
 

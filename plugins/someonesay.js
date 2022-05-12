dd0f360f2119c74caf619cab21325e2d183f89c3
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-12 13:38:08 +0530

    Update someonesay.js

--- plugins/someonesay.js
+++ plugins/someonesay.js
@@ -1,7 +1,7 @@
 
 
 const Asena = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
+const { MessageType, MessageOptions, Mimetype } = require('@ZIYAN-SER/WhatsBixby-Web-Api');
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');

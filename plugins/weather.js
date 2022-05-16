820022581a3d7b663a42846e443ab78e2a639a4c
MIDU-SER <98685114+MIDU-SER@users.noreply.github.com>
2022-05-16 21:04:20 +0530

    Fixxx

--- plugins/weather.js
+++ plugins/weather.js
@@ -7,7 +7,7 @@ WhatsAsena - Yusuf Usta
 */
 
 const Asena = require('../events');
-const {MessageType} = require('@ZIYAN-SER/WhatsBixby-Web-Api');
+const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
 const Config = require('../config');
 

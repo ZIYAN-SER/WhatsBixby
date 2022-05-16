821bf01f27c2aaa81c5069aed4fad68a9a5d797f
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-16 23:40:49 +0530

    Update bgmlist.js

--- plugins/bgmlist.js
+++ plugins/bgmlist.js
@@ -5,7 +5,7 @@ https://github.com/RIPPER-SER/bixbymowl
 */
 
 const Bixby = require('../events');
-const {MessageType, MessageOptions, Mimetype} = require('@ZIYAN-SER/WhatsBixby-Web-Api');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
 const Config = require('../config');
 const fs = require('fs');

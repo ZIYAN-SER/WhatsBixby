f2dd61272966a4cc3b0a06977ce976bc778fd241
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-16 23:48:33 +0530

    Update cr7.js

--- plugins/cr7.js
+++ plugins/cr7.js
@@ -2,7 +2,7 @@
  */
 
 const Asena = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@ZIYAN-SER/WhatsBixby-Web-Api');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');

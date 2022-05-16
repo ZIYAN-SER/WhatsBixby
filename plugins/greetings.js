06e161930dd0ddcd9b0948d078d3963a5768ac50
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-16 23:55:10 +0530

    Update greetings.js

--- plugins/greetings.js
+++ plugins/greetings.js
@@ -7,7 +7,7 @@ WhatsAsena - Yusuf Usta
 */
 
 const Asena = require('../events');
-const {MessageType} = require('@ZIYAN-SER/WhatsBixby-Web-Api');
+const {MessageType} = require('@adiwajshing/baileys');
 const sql = require('./sql/greetings');
 const Config = require('../config');
 

b3265de32541de4e91d0cd35ebbed1be5789547a
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-16 21:11:07 +0530

    Update Play.js

--- plugins/Play.js
+++ plugins/Play.js
@@ -8,7 +8,7 @@ https://github.com/RIPPER-SER/Bixby-Mowl
 //BASIC REQUIREMENTS
 const Bixby = require('../events');
 
-const { MessageType, Mimetype } = require('@ZIYAN-SER/WhatsBixby-Web-Api');
+const { MessageType, Mimetype } = require('@adiwajshing/baileys');
 
 const got = require('got');
 

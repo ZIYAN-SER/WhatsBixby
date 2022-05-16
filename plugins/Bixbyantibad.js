08d2e6f5918ee28a3a1f788b67edb641eac1e915
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-16 21:07:49 +0530

    Update Bixbyantibad.js

--- plugins/Bixbyantibad.js
+++ plugins/Bixbyantibad.js
@@ -5,7 +5,7 @@ https://github.com/ZIYAN-SER/WhatsBixby
 */
 
 const Bixby = require('../events');
-const {MessageType} = require('@ZIYAN-SER/WhatsBixby-Web-Api');
+const {MessageType} = require('@adiwajshing/baileys');
 const exec = require('child_process').exec;
 const os = require("os");
 const fs = require('fs');

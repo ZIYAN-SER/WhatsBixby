361fb086d1cb3ef018cbe0eef58329c04446a9fa
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-16 21:07:19 +0530

    FIX✔️

--- plugins/Anime_Maker.js
+++ plugins/Anime_Maker.js
@@ -10,7 +10,7 @@ https://github.com/ZIYAN-SER/WhatsBixby
 
 //BASIC REQUIREMENTS
 const WhatsBixby = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@ZIYAN-SER/WhatsBixby-Web-Api');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');

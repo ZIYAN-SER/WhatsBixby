14ac547cc140ba5516faa7a3b79b1757e469e71f
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-16 21:09:45 +0530

    Update Gaymeter.js

--- plugins/Gaymeter.js
+++ plugins/Gaymeter.js
@@ -2,7 +2,7 @@
 
 const Asena = require('../events');
 const con = require('../config');
-const {MessageType, MessageOptions, Mimetype} = require('@ZIYAN-SER/WhatsBixby-Web-Api');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const fs = require('fs');
 
 // Descriptions

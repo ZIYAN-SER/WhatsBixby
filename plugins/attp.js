f71b08abfc6e075c2d0eddb76fee976b77c113c1
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-05-16 23:38:28 +0530

    Update attp.js

--- plugins/attp.js
+++ plugins/attp.js
@@ -9,7 +9,7 @@ https://github.com/RIPPER-SER/bixbymowl
 
 //BASIC REQUIREMENTS
 const Bixby = require('../events');
-const { MessageType, MessageOptions, Mimetype } = require('@ZIYAN-SER/WhatsBixby-Web-Api');
+const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const axios = require('axios');
 const Config = require('../config');

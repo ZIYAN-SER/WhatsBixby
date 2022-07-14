57ba64be199a08941634328df67ef0730b809100
ZIYAN <92292759+ZIYAN-SER@users.noreply.github.com>
2022-07-14 14:42:02 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const { Sequelize } = require('sequelize');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
+DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
+module.exports = {
+    VERSION: 'V 2.0.1',
+    ALIVE: process.env.ALIVE || "https://telegra.ph/file/a590761ce530913ed8af5.jpg Hey {sender}, I'm alive \n Uptime: {uptime}",
+    BLOCK_CHAT: process.env.BLOCK_CHAT || '',
+    ALWAYS_ONLINE: convertToBool(process.env.ALWAYS_ONLINE) || true,
+    READ_MESSAGES: convertToBool(process.env.READ_MESSAGES) || false,
+    PMB_VAR: convertToBool(process.env.PMB_VAR) || false,
+    PMB: process.env.PMB || '* ᴘᴍ ᴍᴇssᴀɢᴇ ɴᴏᴛ ᴀʟʟᴏᴡ .ɪᴀᴍ ʙʟᴏᴄᴋɪɴɢ ʙʀᴏ sᴏʀʀʏ!*',
+    READ_COMMAND: convertToBool(process.env.READ_COMMAND) || true,
+    USERNAME: process.env.USERNAME || '',
+    SESSION: process.env.SESSION || '',
+    IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f","deb80cd12ababea1c9b9a8ad6ce3fab2","78c84c62b32a88e86daf87dd509a657a"],
+    RG: process.env.RG || '919074309534-1632403322@g.us',
+    PASSWORD: process.env.PASSWORD || '',
+    BOT_INFO: process.env.BOT_INFO || 'ᴀʙᴜ-ᴍᴅ;ᴊsʟ;917025994178;https://i.imgur.com/OseHc3b.jpg;https://chat.whatsapp.com/KDt0eEkGCho7tLbfcmMltB',
+    RBG_KEY: process.env.RBG_KEY || '',
+    ALLOWED: process.env.ALLOWED || '91,94,212',
+    CHATBOT: process.env.CHATBOT || 'off',
+    HANDLERS: process.env.HANDLERS || '.,',
+    STICKER_DATA: process.env.STICKER_DATA,
+    BOT_NAME: process.env.BOT_NAME || 'ᴀʙᴜ-ᴍᴅ',
+    AUDIO_DATA: process.env.AUDIO_DATA === undefined || process.env.AUDIO_DATA === "private" ? 'ᴀʙᴜ-sᴇʀ;ᴊsʟ-ᴀʙᴜ;https://i.imgur.com/OseHc3b.jpg' : process.env.AUDIO_DATA,
+    TAKE_KEY: process.env.TAKE_KEY || '',
+    MODE: process.env.MODE || 'private',
+    WARN: process.env.WARN || '4',
+    ANTILINK_WARN: process.env.ANTILINK_WARN || '',
+    HEROKU: {
+        HEROKU: process.env.HEROKU === undefined ? false : convertToBool(process.env.HEROKU),
+        API_KEY: process.env.HEROKU_API_KEY || '',
+        APP_NAME: process.env.HEROKU_APP_NAME || ''
+    },
+    DATABASE_URL: DATABASE_URL,
+    DATABASE: DATABASE_URL === './bot.db' ? new Sequelize({ dialect: "sqlite", storage: DATABASE_URL, logging: DEBUG }) : new Sequelize(DATABASE_URL, { dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }, logging: DEBUG }),
+    SUDO: process.env.SUDO || '919446072492',
+    LANGUAGE: process.env.LANGUAGE || 'english',
+    DEBUG: DEBUG,
+    ACR_A: "ff489a0160188cf5f0750eaf486eee74",
+    ACR_S: "ytu3AdkCu7fkRVuENhXxs9jsOW4YJtDXimAWMpJp"
+    };

cd47509e8331cad503325f46ac93123bd265e576
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-07-14 14:19:35 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,14 +3,14 @@ Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 https://github.com/RIPPER-SER/Bixby-Mowl
 */
-//BASIC REQUIREMENTS 
+//BASIC REQUIREMENTS
 const fs = require("fs");
 const path = require("path");
 const events = require("./events");
 const chalk = require('chalk');
 const config = require('./config');
 const simpleGit = require('simple-git');
-const {makeWASocket, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
+const {makeWASocket, DisconnectReason, MessageOptions, MessageType, Mimetype, Presence} = require('@adiwajshing/baileys');
 const {Message, StringSession, Image, Video} = require('./BIXBY/');
 const { DataTypes } = require('sequelize');
 const { getMessage } = require("./plugins/sql/greetings");
